2d2910001c986cd557758c013ac80d1125b8610d
hide24 <hidetoshi.yoshimoto@gmail.com>
2019-04-05 17:42:55 +0900

    separate configuration values from disp-ds.js.

--- app/helpers/disp-ds.js
+++ app/helpers/disp-ds.js
@@ -1,78 +1,42 @@
 import { helper } from '@ember/component/helper';
 import { htmlSafe } from '@ember/template';
 import $ from 'jquery';
+import dsconfig from './dsconfig';
 
-var wayf_URL = "https://test-ds.gakunin.nii.ac.jp/WAYF";
-var wayf_sp_entityID = "https://accounts.test.rdm.nii.ac.jp/shibboleth-sp";
-var wayf_sp_handlerURL = "https://accounts.test.rdm.nii.ac.jp/Shibboleth.sso";
-var wayf_return_url = "https://accounts.test.rdm.nii.ac.jp/login?service=https://test.rdm.nii.ac.jp/";
-var wayf_width = 430;
-var wayf_height = "auto";
-var wayf_show_remember_checkbox = false;
-var wayf_force_remember_for_session = false;
-var wayf_use_small_logo = true;
-var wayf_hide_logo = true;
-var wayf_show_categories = false;
-var wayf_overwrite_intro_text="";
-var wayf_font_size = 11;
-var wayf_font_color = '#000000';
-var wayf_border_color = '#00247D';
-var wayf_background_color = '#F4F7F7';
-var wayf_auto_login = true;
-var wayf_hide_after_login = false;
-var wayf_hide_categories =  new Array("all");
-var wayf_unhide_idps = new Array(
-      "https://openidp.nii.ac.jp/idp/shibboleth",
-      "https://idp.rdm.nii.ac.jp/idp/shibboleth",
-      "https://shib.nagoya-u.ac.jp/idp/shibboleth",
-      "https://authidp1.iimc.kyoto-u.ac.jp/idp/shibboleth"
-);
-var wayf_additional_idps = [
-      {name:"OpenIdP",
-      entityID:"https://openidp.nii.ac.jp/idp/shibboleth",
-      SAML1SSOurl:"https://openidp.nii.ac.jp/idp/profile/Shibboleth/SSO"},
-      {name:"GakuNin RDM IdP",
-      entityID:"https://idp.rdm.nii.ac.jp/idp/shibboleth",
-      SAML1SSOurl:"https://idp.rdm.nii.ac.jp/idp/profile/Shibboleth/SSO"},
-];
-var wayf_sp_cookie_path = "/";
-var wayf_list_height = 350;
+var wayf_overwrite_submit_button_text = dsconfig['wayf_overwrite_submit_button_text'];
+var wayf_overwrite_checkbox_label_text = dsconfig['wayf_overwrite_checkbox_label_text'];
 
+var wayf_sp_entityID = dsconfig['wayf_sp_entityID'];
+var wayf_URL = dsconfig['wayf_URL'];
+var wayf_return_url = dsconfig['wayf_return_url'];
+var wayf_sp_handlerURL = dsconfig['wayf_sp_handlerURL'];
 
+var wayf_use_discovery_service = dsconfig['wayf_use_discovery_service'];
+var wayf_use_small_logo = dsconfig['wayf_use_small_logo'];
+var wayf_width = dsconfig['wayf_width'];
+var wayf_height = dsconfig['wayf_height'];
+var wayf_background_color = dsconfig['wayf_background_color'];
+var wayf_border_color = dsconfig['wayf_border_color'];
+var wayf_font_color = dsconfig['wayf_font_color'];
+var wayf_font_size = dsconfig['wayf_font_size'];
+var wayf_hide_logo = dsconfig['wayf_hide_logo'];
+var wayf_auto_login = dsconfig['wayf_auto_login'];
+var wayf_logged_in_messsage = dsconfig['wayf_logged_in_messsage'];
+var wayf_hide_after_login = dsconfig['wayf_hide_after_login'];
+var wayf_most_used_idps = dsconfig['wayf_most_used_idps'];
+var wayf_show_categories = dsconfig['wayf_show_categories'];
+var wayf_hide_categories = dsconfig['wayf_hide_categories'];
+var wayf_hide_idps = dsconfig['wayf_hide_idps'];
+var wayf_unhide_idps = dsconfig['wayf_unhide_idps'];
+var wayf_show_remember_checkbox = dsconfig['wayf_show_remember_checkbox'];
+var wayf_force_remember_for_session = dsconfig['wayf_force_remember_for_session'];
+var wayf_additional_idps = dsconfig['wayf_additional_idps'];
+var wayf_discofeed_url = dsconfig['wayf_discofeed_url'];
+var wayf_sp_cookie_path = dsconfig['wayf_sp_cookie_path'];
+var wayf_list_height = dsconfig['wayf_list_height'];
+var wayf_sp_samlDSURL = dsconfig['wayf_sp_samlDSURL'];
+var wayf_sp_samlACURL = dsconfig['wayf_sp_samlACURL'];
 
-var wayf_overwrite_submit_button_text;
-var wayf_overwrite_checkbox_label_text;
-
-//var wayf_sp_entityID;
-//var wayf_URL;
-//var wayf_return_url;
-//var wayf_sp_handlerURL;
-
-var wayf_use_discovery_service;
-//var wayf_use_small_logo;
-//var wayf_width;
-//var wayf_height;
-//var wayf_background_color;
-//var wayf_border_color;
-//var wayf_font_color;
-//var wayf_font_size;
-//var wayf_hide_logo;
-//var wayf_auto_login;
-var wayf_logged_in_messsage;
-//var wayf_hide_after_login;
-var wayf_most_used_idps;
-//var wayf_show_categories;
-//var wayf_hide_categories;
-var wayf_hide_idps;
-//var wayf_unhide_idps;
-//var wayf_show_remember_checkbox;
-//var wayf_force_remember_for_session;
-//var wayf_additional_idps;
-var wayf_discofeed_url;
-//var wayf_sp_cookie_path;
-//var wayf_list_height;
-var wayf_sp_samlDSURL;
-var wayf_sp_samlACURL;
 var wayf_html = "";
 
 var wayf_idps = { 
@@ -1701,6 +1665,7 @@ function decodeBase64(input) {
 }
 
 export function dispDs(disp) {
+    console.log(dsconfig);
   if(disp != 'true'){
     start();
     $("#IdPList").on('submit', submit);
@@ -4589,59 +4554,9 @@ export function dispDs(disp) {
   writeHTML('</div>');
   writeHTML('<div style="clear:both;"></div>');
   
-  wayf_html += `
-<style>  
-div#wayfInMenu {
- float: left !important;
-}
-div#wayf_div {
- padding: 5px !important;
- width: auto !important;
- max-width: 430px !important;
-}
-.wayf_col .wayf_col {
- flex-direction: row !important;
- padding-left: 0px !important;
- padding-right: 0px !important;
-}
-div.wayf_radioArea {
- display: none !important;
-}
-div.wayf_row {
- padding-left: 5px !important;
- padding-right: 5px !important;
-}
-div.wayf_linkArea {
- align-content: center !important;
-}
-form#IdPList table tbody {
- display: flex !important;
- flex-direction: row !important;
-}
-form#IdPList table tbody tr td div#clear_a {
- text-decoration: none;
- margin-top: 10px;
- margin-left: 5px;
- margin-right: 3px;
-}
-@media only screen and (max-width: 768px) {
- #wayf_div .wayf_inputArea {
-  flex-wrap: nowrap !important;
- }
- #wayf_submit_div {
-  width: auto !important;
- }
-}
-@media only screen and (max-width: 767px) {
- #wayf_div .wayf_inputArea {
-  flex-wrap: wrap !important;
- }
- #wayf_submit_div {
-  width: 100% !important;
- }
-}
-</style>
-  `;
+  if (typeof(dsconfig['additional_html']) != "undefined" && dsconfig['additional_html'] != ''){
+    wayf_html += dsconfig['additional_html'];
+  };
 
   // Now output HTML all at once
   return htmlSafe(wayf_html);
