9527ee380255eee43181506ba4f70859d7ec5d02
hide24 <hidetoshi.yoshimoto@gmail.com>
2019-04-08 17:36:06 +0900

    update environment.js.

--- config/environment.js
+++ config/environment.js
@@ -68,97 +68,6 @@ const {
     EMBEDDEDDS_URL: dsUrl = '',
 } = { ...process.env, ...localConfig };
 
-var dsconfig = {
-  wayf_URL: "https://ds.gakunin.nii.ac.jp/WAYF",
-  wayf_sp_entityID: "https://accounts.rdm.nii.ac.jp/shibboleth-sp",
-  wayf_sp_handlerURL: "https://accounts.rdm.nii.ac.jp/Shibboleth.sso",
-  wayf_return_url: "https://accounts.rdm.nii.ac.jp/login?service=https://rdm.nii.ac.jp/",
-  wayf_width: 430,
-  wayf_height: "auto",
-  wayf_show_remember_checkbox: false,
-  wayf_force_remember_for_session: false,
-  wayf_use_small_logo: true,
-  wayf_hide_logo: true,
-  wayf_show_categories: false,
-  wayf_overwrite_intro_text: "",
-  wayf_font_size: 11,
-  wayf_font_color: '#000000',
-  wayf_border_color: '#00247D',
-  wayf_background_color: '#F4F7F7',
-  wayf_auto_login: true,
-  wayf_hide_after_login: false,
-  wayf_hide_categories:  new Array("all"),
-  wayf_unhide_idps: new Array(
-      "https://openidp.nii.ac.jp/idp/shibboleth",
-      "https://idp.rdm.nii.ac.jp/idp/shibboleth",
-      "https://shib.nagoya-u.ac.jp/idp/shibboleth",
-      "https://authidp1.iimc.kyoto-u.ac.jp/idp/shibboleth"
-),
-  wayf_additional_idps: [
-      {name: "OpenIdP",
-      entityID: "https://openidp.nii.ac.jp/idp/shibboleth",
-      SAML1SSOurl: "https://openidp.nii.ac.jp/idp/profile/Shibboleth/SSO"},
-      {name: "GakuNin RDM IdP",
-      entityID: "https://idp.rdm.nii.ac.jp/idp/shibboleth",
-      SAML1SSOurl: "https://idp.rdm.nii.ac.jp/idp/profile/Shibboleth/SSO"},
-],
-  wayf_sp_cookie_path: "/",
-  wayf_list_height: 350,
-  additional_html: `
-<style>  
-div#wayfInMenu {
- float: left !important;
-}
-div#wayf_div {
- padding: 5px !important;
- width: auto !important;
- max-width: 430px !important;
-}
-.wayf_col .wayf_col {
- flex-direction: row !important;
- padding-left: 0px !important;
- padding-right: 0px !important;
-}
-div.wayf_radioArea {
- display: none !important;
-}
-div.wayf_row {
- padding-left: 5px !important;
- padding-right: 5px !important;
-}
-div.wayf_linkArea {
- align-content: center !important;
-}
-form#IdPList table tbody {
- display: flex !important;
- flex-direction: row !important;
-}
-form#IdPList table tbody tr td div#clear_a {
- text-decoration: none;
- margin-top: 10px;
- margin-left: 5px;
- margin-right: 3px;
-}
-@media only screen and (max-width: 768px) {
- #wayf_div .wayf_inputArea {
-  flex-wrap: nowrap !important;
- }
- #wayf_submit_div {
-  width: auto !important;
- }
-}
-@media only screen and (max-width: 767px) {
- #wayf_div .wayf_inputArea {
-  flex-wrap: wrap !important;
- }
- #wayf_submit_div {
-  width: 100% !important;
- }
-}
-</style>
-  `
-};
-
 module.exports = function(environment) {
     const devMode = environment !== 'production';
 
@@ -384,6 +293,97 @@ module.exports = function(environment) {
         },
 
         defaultProvider: 'osf',
+        dsconfig: {
+          wayf_URL: "https://ds.gakunin.nii.ac.jp/WAYF",
+          wayf_sp_entityID: "https://accounts.rdm.nii.ac.jp/shibboleth-sp",
+          wayf_sp_handlerURL: "https://accounts.rdm.nii.ac.jp/Shibboleth.sso",
+          wayf_return_url: "https://accounts.rdm.nii.ac.jp/login?service=https://rdm.nii.ac.jp/",
+          wayf_width: 430,
+          wayf_height: "auto",
+          wayf_show_remember_checkbox: false,
+          wayf_force_remember_for_session: false,
+          wayf_use_small_logo: true,
+          wayf_hide_logo: true,
+          wayf_show_categories: false,
+          wayf_overwrite_intro_text: "",
+          wayf_font_size: 11,
+          wayf_font_color: '#000000',
+          wayf_border_color: '#00247D',
+          wayf_background_color: '#F4F7F7',
+          wayf_auto_login: true,
+          wayf_hide_after_login: false,
+          wayf_hide_categories:  new Array("all"),
+          wayf_unhide_idps: new Array(
+            "https://openidp.nii.ac.jp/idp/shibboleth",
+            "https://idp.rdm.nii.ac.jp/idp/shibboleth",
+            "https://shib.nagoya-u.ac.jp/idp/shibboleth",
+            "https://authidp1.iimc.kyoto-u.ac.jp/idp/shibboleth"
+         ),
+         wayf_additional_idps: [
+           {name: "OpenIdP",
+              entityID: "https://openidp.nii.ac.jp/idp/shibboleth",
+              SAML1SSOurl: "https://openidp.nii.ac.jp/idp/profile/Shibboleth/SSO"},
+           {name: "GakuNin RDM IdP",
+              entityID: "https://idp.rdm.nii.ac.jp/idp/shibboleth",
+              SAML1SSOurl: "https://idp.rdm.nii.ac.jp/idp/profile/Shibboleth/SSO"},
+         ],
+         wayf_sp_cookie_path: "/",
+         wayf_list_height: 350,
+         additional_html: `
+<style>  
+div#wayfInMenu {
+ float: left !important;
+}
+div#wayf_div {
+ padding: 5px !important;
+ width: auto !important;
+ max-width: 430px !important;
+}
+.wayf_col .wayf_col {
+ flex-direction: row !important;
+ padding-left: 0px !important;
+ padding-right: 0px !important;
+}
+div.wayf_radioArea {
+ display: none !important;
+}
+div.wayf_row {
+ padding-left: 5px !important;
+ padding-right: 5px !important;
+}
+div.wayf_linkArea {
+ align-content: center !important;
+}
+form#IdPList table tbody {
+ display: flex !important;
+ flex-direction: row !important;
+}
+form#IdPList table tbody tr td div#clear_a {
+ text-decoration: none;
+ margin-top: 10px;
+ margin-left: 5px;
+ margin-right: 3px;
+}
+@media only screen and (max-width: 768px) {
+ #wayf_div .wayf_inputArea {
+  flex-wrap: nowrap !important;
+ }
+ #wayf_submit_div {
+  width: auto !important;
+ }
+}
+@media only screen and (max-width: 767px) {
+ #wayf_div .wayf_inputArea {
+  flex-wrap: wrap !important;
+ }
+ #wayf_submit_div {
+  width: 100% !important;
+ }
+}
+</style>
+        `
+      }
+
     };
 
     if (environment === 'development') {
@@ -439,8 +439,6 @@ module.exports = function(environment) {
         ENV.metricsAdapters[0].config.id = ENV.metricsAdapters[0].config.id || 'UA-84580271-1';
         ENV.FB_APP_ID = ENV.FB_APP_ID || '1039002926217080';
     }
-
-    ENV.dsconfig = dsconfig;
 
     return ENV;
 };
