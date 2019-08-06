2f3b289c0e93718af36f2ee7b5f0e7f645e6a067
fujimoto <fujimoto@dhcp-200-009.soum.co.jp>
2019-08-06 18:41:23 +0900

    Fix name of model

--- app/models/node.ts
+++ app/models/node.ts
@@ -115,7 +115,7 @@ export default class NodeModel extends BaseFileItem.extend(Validations, Collecta
 
     @hasMany('institution', { inverse: 'nodes' })
     affiliatedInstitutions!: DS.PromiseManyArray<InstitutionModel> | InstitutionModel[];
-    @belongsTo('user') creator!: DS.PromiseObject<User> & UserModel;
+    @belongsTo('user') creator!: DS.PromiseObject<UserModel> & UserModel;
     @attr('number') quotaRate!: number;
     @attr('number') quotaThreshold!: number;
 
