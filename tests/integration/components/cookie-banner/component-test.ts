1816b37cccbbedd3e83cd47cdbb13184425bc6c5
west1636 <nishikawa@nii.ac.jp>
2020-06-01 11:57:56 +0900

    resolve travis error of #18916(2)

--- tests/integration/components/cookie-banner/component-test.ts
+++ tests/integration/components/cookie-banner/component-test.ts
@@ -14,6 +14,6 @@ module('Integration | Component | cookie-banner', hooks => {
                 'If you intend to use the GakuNin RDM, please read our Privacy Policy and information on NII use of cookies. ' +
                 'By clicking Accept or continuing to use our website, you consent to the cookies use on this site. ' +
                 'If you do not agree with the cookies use, please change your browser settings to disable cookies or stop using our website. ' +
-                'Please be aware that you may not be able to use some functions on our website when you have cookies disabled.');
+                'Please be aware that you may not be able to use some functions on our website when you have cookies disabled. Accept');
     });
 });
