d98483a2e3db5bb069d22c007a30a0f2bd6b9f7d
west1636 <nishikawa@nii.ac.jp>
2020-11-02 11:28:22 +0900

    #20044対応6-3

--- app/locales/ja-jp/translations.ts
+++ app/locales/ja-jp/translations.ts
@@ -1,18 +1,1 @@
-/* tslint:disable:object-literal-sort-keys max-line-length */
-import config from 'ember-get-config';
-
-const {
-    OSF: {
-        pageName,
-        longBrand,
-    },
-    signUpPolicy: {
-        privacyPolicyLink,
-        cookiesLink,
-    },
-    navbar: {
-        useSearch,
-    },
-} = config;
-
 export default {};
