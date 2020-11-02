31afaa9f497cebfc3193dd429faa6c5fd2e3ab28
west1636 <nishikawa@nii.ac.jp>
2020-11-02 11:00:18 +0900

    #20044対応6-2

---
+++ app/instance-initializers/i18n.js
@@ -0,0 +1,21 @@
+
+export default {
+  name: 'i18n',
+  initialize: function({ container }) {
+    let i18n = container.lookup('service:i18n');
+    
+    i18n.addTranslations('ja-jp');
+//    i18n.set('locale', calculateLocale(i18n.get('locales')));
+  }
+}
+
+function calculateLocale(locales) {
+  // whatever you do to pick a locale for the user:
+  const language = navigator.languages[0] || navigator.language || navigator.userLanguage;
+
+  return locales.includes(language.toLowerCase()) ? language : 'en-GB';
+}
+
+this.get('i18n').addTranslations('en', {
+  'user.profile.gravatar.help': 'Manage your avatar at gravatar.com.'
+});
