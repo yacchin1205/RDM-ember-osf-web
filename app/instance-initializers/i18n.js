340e72a3d5130095e02336aed2da35a63bc162f0
soum-fujimoto <fujimoto@soum.co.jp>
2021-04-05 07:26:35 +0000

    Fix TravisCI error

--- app/instance-initializers/i18n.js
+++ app/instance-initializers/i18n.js
@@ -1,19 +1,17 @@
+function calculateLocale(locales) {
+    // whatever you do to pick a locale for the user:
+    const language = (navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage;
+    const fallbackCode = language.split('-')[0];
+
+    return locales.includes(language.toLowerCase()) ? language : fallbackCode;
+}
 
 export default {
     name: 'i18n',
-    initialize: function(app) {
+    initialize(app) {
         const i18n = app.lookup('service:i18n');
 
         i18n.addTranslations('ja-jp');
         i18n.set('locale', calculateLocale(i18n.get('locales')));
     },
 };
-
-function calculateLocale(locales) {
-    // whatever you do to pick a locale for the user:
-    const defaultLanguage = 'en';
-    const language = (navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage;
-    const fallbackCode = language.split('-')[0];
-
-    return locales.includes(language.toLowerCase()) ? language : fallbackCode;
-}
--- tests/integration/components/cookie-banner/component-test.ts
+++ tests/integration/components/cookie-banner/component-test.ts
@@ -11,12 +11,12 @@ module('Integration | Component | cookie-banner', hooks => {
 
         assert.dom(this.element)
             .hasText('GakuNin RDM is a service provided by the National Institute of Informatics (NII) '
-		+ 'on trust from user organizations, and the terms of use and privacy policy of '
-		+ 'each user organization are applied. GakuNin RDM uses cookies to operate the service '
-		+ 'and improve the user experience. Users of GakuNin RDM are requested to read the '
-		+ 'information on our privacy policy. By clicking "I agree" or continuing to use this '
-		+ 'site, you agree to our use of cookies. If you disagree, please disable cookies in '
-		+ 'your browser settings or discontinue using this site. Please note that by not using '
-		+ 'cookies, you may not be able to use some of this website\'s functions. Accept');
+                + 'on trust from user organizations, and the terms of use and privacy policy of '
+                + 'each user organization are applied. GakuNin RDM uses cookies to operate the service '
+                + 'and improve the user experience. Users of GakuNin RDM are requested to read the '
+                + 'information on our privacy policy. By clicking "I agree" or continuing to use this '
+                + 'site, you agree to our use of cookies. If you disagree, please disable cookies in '
+                + 'your browser settings or discontinue using this site. Please note that by not using '
+                + 'cookies, you may not be able to use some of this website\'s functions. Accept');
     });
 });
