1a058bdda1e7792fe3158fd09797e233f2406a70
soum-fujimoto <fujimoto@soum.co.jp>
2021-06-15 09:06:36 +0000

    Modify layout

--- tests/acceptance/logged-out-homepage-test.ts
+++ tests/acceptance/logged-out-homepage-test.ts
@@ -1,4 +1,4 @@
-import { currentRouteName, currentURL, settled, visit } from '@ember/test-helpers';
+import { currentURL, settled, visit } from '@ember/test-helpers';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import Features from 'ember-feature-flags';
@@ -7,7 +7,7 @@ import { t } from 'ember-intl/test-support';
 import { percySnapshot } from 'ember-percy';
 import { module, test } from 'qunit';
 
-import { click, setupOSFApplicationTest } from 'ember-osf-web/tests/helpers';
+import { setupOSFApplicationTest } from 'ember-osf-web/tests/helpers';
 
 const { featureFlagNames: { ABTesting } } = config;
 
@@ -29,46 +29,6 @@ module('Acceptance | logged-out home page test', hooks => {
         assert.dom('[data-test-hero-subheading]')
             .containsText(t('osf-components.hero-banner.subheading').toString());
 
-        // Check support
-        assert.dom('[data-test-support-heading]').hasText('How GakuNin RDM supports your research');
-        assert.dom('[data-test-support-search]').exists();
-        assert.dom('[data-test-support-design]').exists();
-        assert.dom('[data-test-support-analyze]').exists();
-        assert.dom('[data-test-support-publish]').exists();
-        assert.dom('[data-test-arrow]').exists({ count: 3 });
-        assert.dom('[data-test-learn-more-button]').exists();
-
-        // Check carousel
-        assert.dom('[data-test-testimonials-container]').exists();
-        assert.dom('[data-test-testimonials-heading]').hasText('What others are saying');
-        assert.dom('[data-test-testimonials-container]').exists();
-        // check for 3 carousel slides
-        assert.dom('[data-test-testimonials-slide-1]').exists();
-
-        await percySnapshot('Acceptance | logged-out home page test | carousel exists');
-
-        await click('[data-test-carousel-button-next]');
-
-        await percySnapshot('Acceptance | logged-out home page test | next carousel');
-
-        assert.dom('[data-test-testimonials-slide-2]').exists();
-        await click('[data-test-carousel-button-next]');
-        assert.dom('[data-test-testimonials-slide-3]').exists();
-
-        // Check integrations
-        assert.dom('[data-test-integrations-heading]')
-            .containsText(t('new-home.integrations-section.header').toString());
-        assert.dom('[data-test-authentication-heading]')
-            .containsText(t('new-home.integrations-section.authentication').toString());
-        assert.dom('[data-test-discovery-heading]')
-            .containsText(t('new-home.integrations-section.discovery').toString());
-        assert.dom('[data-test-references-heading]')
-            .containsText(t('new-home.integrations-section.references').toString());
-        assert.dom('[data-test-storage-heading]')
-            .containsText(t('new-home.integrations-section.storage').toString());
-        assert.dom('[data-test-logo]').exists({ count: 8 });
-        assert.dom('[data-test-get-started-button]').exists({ count: 1 });
-
         // Check footer
         assert.dom('footer').exists();
         await a11yAudit();
@@ -84,16 +44,8 @@ module('Acceptance | logged-out home page test', hooks => {
         await settled();
         assert.dom('[data-test-hero-heading]')
             .containsText(t('osf-components.hero-banner.headingB').toString());
-        assert.dom('[data-test-get-started-button]').exists({ count: 1 });
 
         await a11yAudit();
         await percySnapshot(assert);
-    });
-
-    test('Get Started button works', async assert => {
-        await visit('/');
-
-        await click('[data-test-get-started-button]');
-        assert.equal(currentRouteName(), 'register', 'current route is register');
     });
 });
